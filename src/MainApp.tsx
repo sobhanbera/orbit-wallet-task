b60541dee97b2cf3b5ec4448bae9741c54e0e5d8
sobhanbera <sobhanbera258@gmail.com>
2024-05-27 17:55:14 +0530

    project dev initialized

---
+++ src/MainApp.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const MainApp = (props: Props) => {
+  return (
+    <View>
+      <Text>MainApp</Text>
+    </View>
+  );
+};
+
+export default MainApp;
